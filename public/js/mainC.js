23c62219d0fc564791d6c79e360ad9a1e7095757
Sabine Rosenberg <sr@intrval.xyz>
2019-11-24 19:40:14 -0500

    initial

---
+++ public/js/mainC.js
@@ -0,0 +1,275 @@
+window.onload = function(){
+console.log("we have launched client script");
+let client = io.connect('http://localhost:4200');
+let shapesAddedToDrawingBoard = [];
+
+
+function Shape(e){
+  this.directionX = 1;
+  this.directionY = 1;
+  this.speedX = Math.floor(Math.random()*4)+1;
+  this.speedY = Math.floor(Math.random()*4)+1;
+  this.shape = e;
+}
+
+
+function runClientInConnect(){
+//let canvas = $("#paper");
+//let context = canvas.getContext('2d');
+$("#a").on("click", function(){
+//  console.log("a clicked");
+ let nRect = $("<div>");
+ $(nRect).addClass("rectA");
+ $(nRect).appendTo("#paper");
+ shapesAddedToDrawingBoard.push(new Shape(nRect));
+
+ let obj = {
+   theClass: "rectA",
+
+ }
+  client.emit("newRectA",obj);
+
+
+})
+
+$("#b").on("click", function(){
+//  console.log("b clicked");
+ let nRect = $("<div>");
+ $(nRect).addClass("rectB");
+ $(nRect).appendTo("#paper");
+ shapesAddedToDrawingBoard.push(new Shape(nRect));
+ let obj = {
+   theClass: "rectB"
+ }
+  client.emit("newRectB",obj);
+
+
+
+})
+
+$("#c").on("click", function(){
+  //console.log("c clicked");
+ let nRect = $("<div>");
+ $(nRect).addClass("rectC");
+ $(nRect).appendTo("#paper");
+ shapesAddedToDrawingBoard.push(new Shape(nRect));
+ let obj = {
+   theClass: "rectC"
+ }
+  client.emit("newRectC",obj);
+
+
+
+})
+
+$("#d").on("click", function(){
+//  console.log("d clicked");
+ let nRect = $("<div>");
+ $(nRect).addClass("rectD");
+ $(nRect).appendTo("#paper");
+ shapesAddedToDrawingBoard.push(new Shape(nRect));
+ let obj = {
+   theClass: "rectD"
+ }
+  client.emit("newRectD",obj);
+})
+
+window.requestAnimationFrame(animate);
+
+function animate(){
+  for(let i =0; i<shapesAddedToDrawingBoard.length; i++ ){
+//    console.log(shapesAddedToDrawingBoard[i]);
+    let x = $(shapesAddedToDrawingBoard[i].shape).css("left");
+    let y = $(shapesAddedToDrawingBoard[i].shape).css("top");
+
+  //  console.log(x);
+    let xPos = parseInt(x.substr(0,x.length-2));
+    let yPos = parseInt(y.substr(0,y.length-2));
+
+ if (xPos> 400 ||xPos<0){
+   shapesAddedToDrawingBoard[i].directionX *=-1;
+  }
+
+  if (yPos> 400 ||yPos<0){
+    shapesAddedToDrawingBoard[i].directionY *=-1;
+   }
+    xPos= xPos+(shapesAddedToDrawingBoard[i].speedX)*shapesAddedToDrawingBoard[i].directionX;
+    yPos= yPos+(shapesAddedToDrawingBoard[i].speedY)*shapesAddedToDrawingBoard[i].directionY;
+    //yPos= xPos+.01;
+
+    let newPosX = xPos.toString()+"px";
+    let newPosY = yPos.toString()+"px";
+
+
+    $(shapesAddedToDrawingBoard[i].shape).css({"left":newPosX,"top":newPosY});
+}
+  window.requestAnimationFrame(animate);
+}
+
+}
+ //client in connect
+
+
+//
+// /* THESE CALLBACKS ARE MESSAGES RECEIVED FROM THE SERVER */
+//  // when receives a connect message
+client.on('connect', function(data) {
+  console.log("I am online");
+ client.emit('join', 'Hello World from client');
+ runClientInConnect();
+});
+
+client.on('err', function (data){
+  console.log("error no connection");
+  client.disconnect();
+  
+
+})
+
+client.on('movingFromServer', function(data) {
+  console.log("got data");
+  console.log(data);
+  let sRect = $("<div>");
+  sRect.addClass(data.theClass);
+
+  shapesAddedToDrawingBoard.push(new Shape(sRect));
+   $(sRect).appendTo("#paper");
+});
+
+// /* check if the color has changed */
+// document.getElementById("cPicker").onchange=function(){
+//   console.log(document.getElementById('cPicker').value);
+//   drawingColor = "#"+document.getElementById('cPicker').value ;
+// }
+// /* check if reset has been pressed - clear canvas */
+// document.getElementById("reset").onclick = function(){
+//   context.clearRect(0, 0, canvas.width, canvas.height);
+//   document.getElementById('cPicker').value = "FF0000";
+//   document.getElementById('wPicker').value = "1";
+//   // force a key up event in order to reset values ...
+//   document.getElementById('cPicker').dispatchEvent(new KeyboardEvent('keyup'));
+//
+// }
+//
+// /* check if stroke weight has been changed */
+// document.getElementById("wPicker").onchange= function(){
+//   strokeWeight = Number(document.getElementById('wPicker').value);
+//   console.log(strokeWeight);
+// }
+//
+//
+// /* reveive message from server that mouse moved (mousemove) */
+// client.on('movingFromServer', function(data) {
+// context.lineTo(Number(data.x), Number(data.y));
+// context.strokeStyle= data.dCol;
+// //set strokeWidth ...
+// context.lineWidth=data.lWidth;
+// context.stroke();
+// });
+// /* reveive message from server that mouse stopped moving (UP) */
+// client.on('receiveStopFromServer', function(data){
+// console.log("receivedStop");
+// tool.started = false;
+// });
+// /* reveive message from server that mouse started moving (DOWN) */
+// client.on('receiveDownFromServer', function(data){
+// console.log("receivedDown");
+// context.beginPath();
+// tool.started = true;
+// });
+//
+// /************** CANVAS DRAWING ************/
+// //set default drawing color
+// let drawingColor = "red";
+// // setDefault stroke Weight
+// let strokeWeight =1;
+// let canvas = document.getElementById("paper");
+// context = canvas.getContext('2d');
+// tool = new tool_pencil();
+//
+// // make a class for a pencil - to track mouse :::
+// // This painting tool works like a drawing
+// // pencil which tracks the mouse movements
+//
+// function tool_pencil () {
+//
+// 	this.started = false;
+//  // eventOnCanvas is the callback function ... when we use mouse on the canvas
+// 	canvas.addEventListener('mousedown',  eventOnCanvas, false);
+// 	canvas.addEventListener('mousemove',  eventOnCanvas, false);
+// 	canvas.addEventListener('mouseup',	  eventOnCanvas, false);
+//
+// 	// This is called when you start holding down the mouse button
+// 	// This starts the pencil drawing
+// 	this.mousedown = function (ev) {
+//     // SEND MESSAGE TO SERVER
+//      client.emit('receiveDown', {'messageD':'go'});
+// 			context.beginPath();
+// 			context.moveTo(ev._x, ev._y);
+// 			this.started = true;
+// 	};
+//
+// 	// This function is called every time you move the mouse. Obviously, it only
+// 	// draws if the tool.started state is set to true (when you are holding down
+// 	// the mouse button)
+// 	this.mousemove = function (ev) {
+// 		if (this.started) {
+//         // SEND MESSAGE TO SERVER
+// 			client.emit('receiveMove',
+//                 {
+//                 'x': ev._x,
+//                 'y': ev._y,
+//                 'dCol': drawingColor,
+//                 'lWidth':strokeWeight
+//                });
+// 			context.lineTo(ev._x, ev._y);
+// 			context.strokeStyle= drawingColor;
+//       context.lineWidth = strokeWeight;
+// 			context.stroke();
+// 		}
+// 	};
+//
+// 	// This is called when you release the mouse button
+// 	this.mouseup = function (ev) {
+// 		if (this.started) {
+// 			//tool.mousemove(ev);
+//       client.emit('receiveStop', {'messageS':'stop'});
+// 			this.started = false;
+// 		}
+// 	};
+//
+// }
+// // The general-purpose event handler. This function just determines
+// // the mouse position relative to the <canvas> element
+// // ev is the event arg (what event occured)
+// function eventOnCanvas (ev) {
+// 	// Firefox and chrome
+// 	if (ev.layerX || ev.layerX == 0) {
+//   // get the absolute pos of the element (canvas)
+// 	 var domRect = document.getElementById("paper").getBoundingClientRect();
+//    // note drawing coordinates start at 0,0 in the canvas - so we need the diff
+//    // between the mouse and where the canvas starts  ...
+//    var diffX =  ev.clientX-domRect.x;
+//    var diffY =  ev.clientY-domRect.y;
+//    ev._x = diffX;
+// 	 ev._y =diffY;
+//  }
+// /*
+// 	 Call the event handler of the within the pencilTool class
+//    could be mousedown, mouseup, mousemove...
+// */
+//   if(ev.type ==="mousedown"){
+//     tool.mousedown(ev);
+//   }
+//   if(ev.type ==="mouseup"){
+//     tool.mouseup(ev);
+//   }
+//   if(ev.type ==="mousemove"){
+//     tool.mousemove(ev);
+//   }
+// }
+//socket on connect
+//});
+
+// window has loaded
+}
---
+++ server.js
@@ -0,0 +1,63 @@
+//import the Express library
+let connectionsLimit = 3
+let express = require('express');
+const portNumber =4200;
+let app = express(); //make an insatnce of express
+let httpServer = require('http').createServer(app);  // create a server (using the Express framework object)
+// make server listen for incoming messages
+httpServer.listen(portNumber, function(){
+  console.log('listening on port:: '+portNumber);
+});
+
+// serving static files
+let static = require('node-static'); // for serving static files (i.e. css,js,html...)
+// serve anything from this dir ...
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+// declare io which mounts to our httpServer object (runs ontop ... )
+let io = require('socket.io')(httpServer);
+// for the client...
+app.use(express.static(__dirname + '/node_modules'));
+
+// Listen for incoming connections from clients
+io.on('connection', function (socket) {
+console.log(io.engine.clientsCount);
+if (io.engine.clientsCount > connectionsLimit) {
+      socket.emit('err', { message: 'reach the limit of connections' })
+      socket.disconnect()
+      console.log('Disconnected...')
+      return;
+    }
+
+socket.on('join', function (data) {
+   console.log("client joined:: "+ data);
+});
+socket.on('newRectA', function (data) {
+    console.log("newRect A:: "+ data);
+    // This line sends the event (broadcasts it)
+    // to everyone except the originating client.
+    socket.broadcast.emit('movingFromServer', data);
+});
+socket.on('newRectB', function (data) {
+    console.log("newRect B:: "+ data);
+    // This line sends the event (broadcasts it)
+    // to everyone except the originating client.
+    socket.broadcast.emit('movingFromServer', data);
+});
+socket.on('newRectC', function (data) {
+    console.log("newRect C:: "+ data);
+    // This line sends the event (broadcasts it)
+    // to everyone except the originating client.
+    socket.broadcast.emit('movingFromServer', data);
+});
+socket.on('newRectD', function (data) {
+    console.log("newRect D:: "+ data);
+    // This line sends the event (broadcasts it)
+    // to everyone except the originating client.
+    socket.broadcast.emit('movingFromServer', data);
+});
+});
